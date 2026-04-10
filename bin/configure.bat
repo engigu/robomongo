b01f14d0e45326a9a3f1dea1fe1af313e8e74202
engigu <sayheya@qq.com>
2026-04-10 15:15:08 +0800

    Build: switch to windows-latest and make configure.bat generator-agnostic for faster GHA builds.

--- bin/configure.bat
+++ bin/configure.bat
@@ -13,7 +13,7 @@ if %ERRORLEVEL% neq 0 (exit /b 1)
 rem Run CMake configuration step
 rem BUILD_TYPE: Release or Debug
 cd "%BUILD_DIR%"
-cmake -G "Visual Studio 15 2017 Win64" -D "CMAKE_PREFIX_PATH=%PREFIX_PATH%" -D "CMAKE_BUILD_TYPE=%BUILD_TYPE%" -D "CMAKE_INSTALL_PREFIX=%INSTALL_PREFIX%" %PROJECT_DIR%
+cmake -D "CMAKE_PREFIX_PATH=%PREFIX_PATH%" -D "CMAKE_BUILD_TYPE=%BUILD_TYPE%" -D "CMAKE_INSTALL_PREFIX=%INSTALL_PREFIX%" %PROJECT_DIR%
 
 @REM echo ___________________________________________________________________
 @REM rem Enable Clang Tidy for Visual Studio 2019 IDE ...
